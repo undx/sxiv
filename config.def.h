e391fedd999944d8d4c2f88cf90f57ddab0ac14b
undx <egallois@talend.com>
2019-09-20 14:05:42 +0200

    add shortcuts, increase thumbs sizes

--- config.def.h
+++ config.def.h
@@ -48,7 +48,15 @@ static const bool ALPHA_LAYER = false;
 #ifdef _THUMBS_CONFIG
 
 /* thumbnail sizes in pixels (width == height): */
-static const int thumb_sizes[] = { 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800, 832, 864, 896, 928, 960, 992, 1024};
+static const int thumb_sizes[] = { 32, 64, 96, 128, 160, 192, 224, 256, 288,
+                                   320, 352, 384, 416, 448, 480, 512, 544, 576,
+                                   608, 640, 672, 704, 736, 768, 800, 832, 864,
+                                   896, 928, 960, 992, 1024, 1056, 1088, 1120,
+                                   1152, 1184, 1216, 1248, 1280, 1312, 1344,
+                                   1376, 1408, 1440, 1472, 1504, 1536, 1568,
+                                   1600, 1632, 1664, 1696, 1728, 1760, 1792,
+                                   1824, 1856, 1888, 1920, 1952, 1984, 2016,
+                                   2048};
 
 /* thumbnail size at startup, index into thumb_sizes[]: */
 static const int THUMB_SIZE = 3;
@@ -101,9 +109,12 @@ static const keymap_t keys[] = {
 	{ 0,            XK_R,             t_reload_all,         None },
 
 	{ 0,            XK_n,             i_navigate,           +1 },
+	{ 0,            XK_Right,         i_navigate,           +1 },
 	{ 0,            XK_n,             i_scroll_to_edge,     DIR_LEFT | DIR_UP },
 	{ 0,            XK_space,         i_navigate,           +1 },
+	{ ShiftMask,    XK_space,         i_navigate,           -1 },
 	{ 0,            XK_p,             i_navigate,           -1 },
+	{ 0,            XK_Left,          i_navigate,           -1 },
 	{ 0,            XK_p,             i_scroll_to_edge,     DIR_LEFT | DIR_UP },
 	{ 0,            XK_BackSpace,     i_navigate,           -1 },
 	{ 0,            XK_bracketright,  i_navigate,           +10 },
@@ -113,13 +124,13 @@ static const keymap_t keys[] = {
 	{ ControlMask,  XK_p,             i_navigate_frame,     -1 },
 	{ ControlMask,  XK_space,         i_toggle_animation,   None },
 	{ 0,            XK_h,             i_scroll,             DIR_LEFT },
-	{ 0,            XK_Left,          i_scroll,             DIR_LEFT },
+	{ ShiftMask,    XK_Left,          i_scroll,             DIR_LEFT },
 	{ 0,            XK_j,             i_scroll,             DIR_DOWN },
-	{ 0,            XK_Down,          i_scroll,             DIR_DOWN },
+	{ ShiftMask,    XK_Down,          i_scroll,             DIR_DOWN },
 	{ 0,            XK_k,             i_scroll,             DIR_UP },
-	{ 0,            XK_Up,            i_scroll,             DIR_UP },
+	{ ShiftMask,    XK_Up,            i_scroll,             DIR_UP },
 	{ 0,            XK_l,             i_scroll,             DIR_RIGHT },
-	{ 0,            XK_Right,         i_scroll,             DIR_RIGHT },
+	{ ShiftMask,    XK_Right,         i_scroll,             DIR_RIGHT },
 	{ 0,            XK_H,             i_scroll_to_edge,     DIR_LEFT },
 	{ 0,            XK_J,             i_scroll_to_edge,     DIR_DOWN },
 	{ 0,            XK_K,             i_scroll_to_edge,     DIR_UP },
